afab4adb0947d5e8e261e2490882cc07a60985fd
SergeiIm <128727366+SergeiIm@users.noreply.github.com>
2024-03-26 07:21:23 +0100

    add autoupgrade version browser

--- conftest.py
+++ conftest.py
@@ -1,6 +1,9 @@
 import pytest
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+
 
 @pytest.fixture(scope="function", autouse=True)
 def driver(request):
@@ -9,7 +12,7 @@ def driver(request):
     options.add_argument("--no-sandbox")
     options.add_argument("--disable-dev-shm-usage")
     options.add_argument("--window-size=1920,1080")
-    driver = webdriver.Chrome(options=options)
+    driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
     request.cls.driver = driver
     yield driver
     driver.quit()
